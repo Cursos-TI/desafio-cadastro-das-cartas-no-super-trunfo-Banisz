6ab744c0e6f144583a27fb229d3b71f21157548e
Banisz <joaobani73@gmail.com>
2025-02-25 12:09:18 +0000

    Tentando fazer funcionar

---
+++ Desafio/CartasSuperTrunfoDesafio.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int População = '2.000.000';
+    float Área km² = 11 401,092 ; 
+    float PIB; = "103.201.000.000";
+    int Turisticos; = '50'; 
+    char estado = 'G';
+    char Cidade = 'Manaus';
+
+
+    printf("O Número da população é: %d\n");
+
+
+
+
+
+
+
+
+
+
+
+}
